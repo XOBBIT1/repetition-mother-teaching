a5441fd2c3b49abb9010c946870eeaa29e93a1ce
37533 <ben300300@gmail.com>
2021-12-13 18:21:54 +0300

    My project

--- myweb_main/admin.py
+++ myweb_main/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 
-from myweb_main.models import Post, Profile
+from myweb_main.models import Post, Profile, HashTeg
 from django.contrib.auth.admin import UserAdmin as UserBaseAdmin
 
 admin.site.register(Post)
-
+admin.site.register(HashTeg)
 
 class ProfileInline(admin.StackedInline):
     model = Profile
@@ -17,3 +17,4 @@ class UserAdmin(UserBaseAdmin):
     inlines = (
         ProfileInline,
     )
+
--- myweb_main/models.py
+++ myweb_main/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from django.db import models
-from django import forms
+from django.db.models import ManyToManyField
 
 
 class Post(models.Model):
@@ -29,3 +29,9 @@ class Profile(models.Model):
     github = models.URLField(max_length=2048, null=True, blank=True)
 
 
+class HashTeg(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    post = models.ManyToManyField(Post)
+
+    def __str__(self):
+        return self.title
